bf306c30fe0e5dde49ac9c996bb447a749af62c4
Zhou Honglin <zhou.honglin@gmail.com>
2012-12-26 12:44:19 +0800

    Added tests: custom cookie test; multipart post test; improved ipsum.js syntax

--- test/fixture/applications/ipsum.js
+++ test/fixture/applications/ipsum.js
@@ -1,9 +1,23 @@
+var debug = require('debug')('carcass:Application:Ipsum');
+
 var carcass = require('carcass');
 
-module.exports = carcass.factories.Express(function(app, options) {
+module.exports = carcass.factories.Express({
+	initialize : initialize
+});
+
+function initialize(app, options) {
+	debug('initializing');
 
     app.post('/post', function(req, res, next) {
         res.send(req.body);
+    });
+
+    app.post('/multipart', function(req, res, next) {
+    	res.json({
+    		'field': req.body.field,
+    		'filename': req.files.file.name
+    	});
     });
 
     app.get('/cookie', function(req, res, next) {
@@ -11,4 +25,7 @@ module.exports = carcass.factories.Express(function(app, options) {
         res.send();
     });
 
-});
+    app.get('/random-cookie', function(req, res, next) {
+    	res.json(req.cookies);
+    });
+}
