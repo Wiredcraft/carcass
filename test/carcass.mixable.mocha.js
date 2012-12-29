7f4687341c9b6f38848c91ea7094573bb647122e
Zhou Honglin <zhou.honglin@gmail.com>
2012-12-29 11:56:35 +0800

    Added mixin descriptor test.

--- test/carcass.mixable.mocha.js
+++ test/carcass.mixable.mocha.js
@@ -117,24 +117,6 @@ describe('Mixin', function() {
         });
     });
 
-    it('should only allow enumerable properties be mixed in.', function(done) {
-        var obj = carcass.mixable();
-        var mix = Object.defineProperties({}, {
-            canenum: { 
-                value: 1,
-                enumerable: true
-            },
-            notenum: {
-                value: 1,
-                enumerable: false
-            }
-        });
-        obj.mixin(mix);
-        obj.should.have.property('canenum', 1);
-        obj.should.not.have.property('notenum');
-        done();
-    });
-
     it('should only allow Function and Object to be mixed in from.', function(done) {
         var num = 1, str = 'string', bool = true;
         var obj = carcass.mixable();
@@ -146,5 +128,98 @@ describe('Mixin', function() {
 });
 
 describe('Mixin descriptor', function() {
-    // TODO: what descriptor is.
+    describe('enumerable', function() {
+            it('should be kept after mixin.', function(done) {
+                var obj = carcass.mixable();
+                var mix = Object.defineProperties({}, {
+                    canenum: { 
+                        value: 1,
+                        enumerable: true
+                    }
+                });
+                obj.mixin(mix);
+                obj.should.have.property('canenum');
+                done();
+            });
+            
+            it('should only allow enumerable properties be mixed in.', function(done) {
+                var obj = carcass.mixable();
+                var mix = Object.defineProperties({}, {
+                    notenum: {
+                        value: 1,
+                        enumerable: false
+                    }
+                });
+                obj.mixin(mix);
+                obj.should.not.have.property('notenum');
+                done();
+            });
+    });
+
+    describe('value', function() {
+        it('should be kept the same after mix.', function(done) {
+            var obj = carcass.mixable();
+            var mix = Object.defineProperties({}, {
+                prop: {
+                    value: 'somevalue',
+                    enumerable: true
+                }
+            });
+            obj.mixin(mix);
+            obj.should.have.property('prop', 'somevalue');
+            done();
+        });
+    });
+
+    describe('writable/configurable', function() {
+        it('should be kept the same after mix.', function(done) {
+            var obj = carcass.mixable();
+            var mix = Object.defineProperties({}, {
+                prop: {
+                    writable: true,
+                    configurable: true,
+                    enumerable: true,
+                    value: 0
+                },
+                prop2: {
+                    writable: false,
+                    configurable: false,
+                    enumerable: true,
+                    value: 0
+                }
+            });
+            obj.mixin(mix);
+            obj.should.have.property('prop');
+            obj.should.have.property('prop2');
+
+            var desc = Object.getOwnPropertyDescriptor(obj, 'prop');
+            desc.writable.should.eql(true);
+            desc.configurable.should.eql(true);
+            
+            var desc = Object.getOwnPropertyDescriptor(obj, 'prop2');
+            desc.writable.should.eql(false);
+            desc.configurable.should.equal(false);
+            done();
+        })
+    });
+
+    describe('get/set', function() {
+        it('should be kept the same after mix.', function(done) {
+            var obj = carcass.mixable();
+            var outval = 'val';
+            var mix = Object.defineProperties({}, {
+                prop: {
+                    get: function() { return outval; },
+                    set: function(val) { outval = val; },
+                    enumerable: true
+                }
+            });
+            
+            obj.mixin(mix);
+            obj.should.have.property('prop', 'val');
+            obj.prop = 'new';
+            obj.prop.should.eql('new');
+            done();
+        })
+    });
 });
