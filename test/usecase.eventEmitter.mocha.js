e08c0d3f7bfefbdff9b5d013b6174bbcd93d2690
Makara Wang <makara15@gmail.com>
2013-01-02 12:47:47 +0800

    Improved event emitter use case.

--- test/usecase.eventEmitter.mocha.js
+++ test/usecase.eventEmitter.mocha.js
@@ -30,12 +30,33 @@ describe('Event Emitter', function() {
     describe('With the mixin method I built', function() {
         it('should also work.', function(done) {
             var lorem = false;
-            var MyStream = carcass.mixable(function() {});
+            var MyStream = carcass.mixable(function() {
+                EventEmitter.call(this);
+            });
             MyStream.prototype.mixin(EventEmitter.prototype);
             MyStream.prototype.write = function(data) {
                 this.emit('data', data);
             };
             var stream = new MyStream();
+            stream.on('data', function(data) {
+                lorem = true;
+                data.should.equal('Lorem');
+            });
+            stream.write('Lorem');
+            lorem.should.equal(true);
+            setTimeout(done, 1);
+        });
+    });
+
+    describe('And it is a simple object', function() {
+        it('should also work.', function(done) {
+            var lorem = false;
+            var stream = carcass.mixable();
+            EventEmitter.call(stream);
+            stream.mixin(EventEmitter.prototype);
+            stream.write = function(data) {
+                this.emit('data', data);
+            };
             stream.on('data', function(data) {
                 lorem = true;
                 data.should.equal('Lorem');
