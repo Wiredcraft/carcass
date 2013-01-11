9cec145f17e7818268b073eda5f362782fbcb1b8
Zhou Honglin <zhou.honglin@gmail.com>
2013-01-11 15:44:54 +0800

    Make cors example complete: Add package.json; remove applications/session.js; makes user login logouy process complete.

--- examples/cors/applications/testSession.js
+++ examples/cors/applications/testSession.js
@@ -5,28 +5,25 @@ var carcass = require('carcass');
 module.exports = carcass.factories.Express(function(app, options) {
     debug('initializing');
 
-    app.use(function(req, res, next) {
-        if (!req.session) {
-            next(new Error('no session'));
+    app.get('/', function(req, res) {
+        res.json({'page': 'index'});
+    });;
+    
+    app.get('/login', function(req, res) {
+        req.session.login = true;
+        return res.json({'login': 'ok'});
+    });
+    
+    app.get('/member', function(req, res) {
+        if (req.session && req.session.login) {
+            res.json({'login': true});
         } else {
-            next();
+            res.json({'login': false});
         }
     });
-    app.get('/', function(req, res, next) {
-        res.json(req.session || false, 200);
-    });
-    app.get('/id', function(req, res, next) {
-        res.json(req.sessionID || false, 200);
-    });
-    app.get('/destroy', function(req, res, next) {
-        req.session.destroy(function(err) {
-            if (err) {
-                return next(err);
-            }
-            if (req.session) {
-                return next(new Error('failed to destroy'));
-            }
-            res.json(true, 200);
-        });
+    
+    app.get('/logout', function(req, res) {
+        delete req.session.login;
+        return res.json({'logout':'ok'})
     });
 });
--- examples/cors/index.js
+++ examples/cors/index.js
@@ -1,20 +1,14 @@
-var carcass     = require('carcass'),
-    express     = require('express'),
-    RedisStore  = require('connect-redis')(express);
+var carcass = require('carcass');
+require('carcass-auth');
 
 // Register applications.
 carcass.register(__dirname, 'applications');
 
-// Expose an HTTP server.
 var server = new carcass.servers.Http();
 
 server.mount('applications/cors');
 server.mount('applications/restify');
-server.mount('session', {
-    store: new RedisStore({
-        prefix: 'carcass-auth-example'
-    })
-});
+server.mount('session');
 server.mount('applications/testSession');
 
 server.start(function() {
