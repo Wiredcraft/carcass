6988a3264d2a426e4ba24e70f12dff8a19256dd9
Zhou Honglin <zhou.honglin@gmail.com>
2013-01-23 21:18:54 +0800

    Added password hashing to examples/auth/user model. #18.

--- examples/auth/models/user.js
+++ examples/auth/models/user.js
@@ -2,6 +2,7 @@ var debug = require('debug')('carcass:Example:Model:User');
 
 var carcass = require('carcass');
 var path = require('path');
+var crypto = require('crypto');
 
 // Stash storage.
 var storage = carcass.storages.stash({
@@ -15,6 +16,21 @@ var builder = carcass.factories.Model({
     },
     storage: storage
 });
+
+builder.prototype.hashPassword = function(algorithm) {
+    var supportHashes = ['md5', 'sha1', 'sha256', 'sha512'];
+    
+    algorithm = supportHashes.indexOf(algorithm) >= 0 
+                    ? algorithm
+                    : 'sha1';
+    
+    var sha =  crypto.createHash(algorithm)
+                     .update(this.password())
+                     .digest('hex');
+                     
+    // Update password.
+    this.password(sha);
+};
 
 builder.use(carcass.plugins.modelSync);
 
