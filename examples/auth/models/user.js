a2580eff20ed6d08070162ce3bf8b0c8ddecc015
xufeng123 <xvfeng123@gmail.com>
2013-02-26 03:07:59 -0500

    password hash example

--- examples/auth/models/user.js
+++ examples/auth/models/user.js
@@ -2,6 +2,7 @@ var debug = require('debug')('carcass:Example:Model:User');
 
 var carcass = require('carcass');
 var path = require('path');
+var passwordHash = require('password-hash');
 
 // Stash storage.
 var storage = carcass.storages.stash({
@@ -15,6 +16,14 @@ var builder = carcass.factories.Model({
     },
     storage: storage
 });
+
+builder.prototype.hashPassword = function(err) {
+  if(!this.password) {
+    return debug(err);
+  } else {
+    this.password = passwordHash.generate(this.password);
+  }
+}
 
 builder.use(carcass.plugins.modelSync);
 
