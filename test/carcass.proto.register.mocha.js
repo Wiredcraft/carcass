eea2f3fcff664ca1f1e483b112c28476ca80002b
Makara Wang <makara15@gmail.com>
2013-04-19 00:39:23 +0800

    Rebuilding register into proto.

---
+++ test/carcass.proto.register.mocha.js
@@ -0,0 +1,11 @@
+var debug = require('debug')('carcass:test');
+
+var carcass = require('carcass');
+var should = require('should');
+
+describe('Carcass / proto / register:', function() {
+
+    it('should be a ...', function() {
+        carcass.proto.should.have.property('register');
+    });
+});
