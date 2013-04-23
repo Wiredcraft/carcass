9bd660dc2a3e866baa173445d2f4df96074a90c0
Makara Wang <makara15@gmail.com>
2013-04-23 23:36:46 +0800

    Added a benchmark for mixin.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 
 bm:
-	@NODE_ENV=test DEBUG=carcass:* node ./benchmark/*.js
+	@NODE_ENV=test DEBUG=carcass:* ./node_modules/.bin/mocha --timeout 120s --slow 60s ./benchmark/*.js
 
 test:
 	@NODE_ENV=test DEBUG=carcass:* ./node_modules/.bin/mocha ./test/*.mocha.js
---
+++ benchmark/proto.mixin.js
@@ -0,0 +1,54 @@
+var debug = require('debug')('carcass:proto:mixin');
+
+var carcass = require('..');
+var Benchmark = require('benchmark');
+
+var mixin = carcass.proto.mixin;
+var extend = require('es5-ext/lib/Object/extend-properties');
+
+var lorem = {
+    lorem: function() {
+        return 'lorem';
+    }
+};
+var ipsum = {
+    ipsum: function() {},
+    dolor: function() {}
+};
+
+// Benchmark
+// ---
+describe('Proto / Mixin:', function() {
+
+    it('benchmarking the speed of mixin.', function(done) {
+        var suite = Benchmark.Suite();
+        suite.add('using mixin.', function() {
+            var obj = {
+                something: 'Lorem ipsum dolor sit amet',
+                mixin: mixin
+            };
+            obj.mixin(lorem).mixin(ipsum);
+        });
+        suite.add('using mixable.', function() {
+            var obj = carcass.mixable({
+                something: 'Lorem ipsum dolor sit amet'
+            });
+            obj.mixin(lorem).mixin(ipsum);
+        });
+        suite.add('using extend-properties.', function() {
+            var obj = {
+                something: 'Lorem ipsum dolor sit amet'
+            };
+            extend(obj, lorem, ipsum);
+        });
+        suite.on('start', function(event) {
+            debug('started');
+        }).on('cycle', function(event) {
+            debug(String(event.target));
+        }).on('complete', function() {
+            done();
+        }).run({
+            'async': true
+        });
+    });
+});
