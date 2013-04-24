e2aa3d86cf62389b20725c1eac0f72c26c8e5aff
Makara Wang <makara15@gmail.com>
2013-04-25 00:08:16 +0800

    Building proto/register.

--- test/carcass.proto.register.mocha.js
+++ test/carcass.proto.register.mocha.js
@@ -3,9 +3,33 @@ var debug = require('debug')('carcass:test');
 var carcass = require('carcass');
 var should = require('should');
 
+var path = require('path');
+var root = path.resolve(__dirname, 'fixture');
+var lorem = require(path.resolve(root, 'applications', 'lorem'));
+
 describe('Carcass / proto / register:', function() {
 
     it('should be a ...', function() {
         carcass.proto.should.have.property('register');
+    });
+
+    describe('...:', function() {
+        var obj = carcass.mixable();
+
+        before(function() {
+            obj.mixin(carcass.proto.register);
+        });
+
+        it('should ...', function() {
+            obj.should.have.property('register');
+        });
+
+        it('should ...', function() {
+            obj.register(root, 'applications');
+            // obj.should.have.property('applications');
+            // obj.applications.should.be.a('object');
+            // obj.applications.should.have.property('lorem');
+            // obj.applications.lorem.should.equal(lorem);
+        });
     });
 });
