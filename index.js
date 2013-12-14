c1027e5fc68d064fbdf30d64f7a12b21796b5f3f
Makara Wang <makara15@gmail.com>
2013-12-14 20:01:09 +0800

    Removed constructors.

--- index.js
+++ index.js
@@ -12,18 +12,8 @@ exports.register(__dirname, 'utils');
 // Shortcuts.
 exports.httpError = exports.utils.httpError;
 
-// Register constructors.
-exports.register(__dirname, 'constructors');
-
 // Register factories.
 exports.register(__dirname, 'factories');
-
-// .
-_.each(exports.constructors, function(obj) {
-    if (!obj.prototype) return;
-    exports.mixable(obj);
-    exports.plugins.extendable(obj);
-});
 
 // Register servers.
 exports.register(__dirname, 'servers');
