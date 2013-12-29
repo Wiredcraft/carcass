03a262b0eea217c986f11c5e32cae9363a149066
Makara Wang <makara15@gmail.com>
2013-12-29 12:33:09 +0800

    Added proto.configManager.

---
+++ test/proto.configManager.mocha.js
@@ -0,0 +1,79 @@
+// var debug = require('debug')('carcass:test');
+
+var mixable = require('../lib/mixable');
+var configProto = require('../lib/proto/configManager');
+var should = require('should');
+
+var path = require('path');
+var eson = require('eson');
+
+var root = path.resolve(__dirname, 'fixture');
+var lorem = path.resolve(root, 'configs', 'lorem.json');
+var ipsum = path.resolve(root, 'configs', 'ipsum.json');
+
+describe('Carcass / proto / config:', function() {
+
+    it('should be a proto', function() {
+        configProto.should.be.a('object');
+    });
+
+    describe('Use:', function() {
+        var obj = mixable();
+        obj.mixin(configProto);
+
+        it('should have some methods', function() {
+            obj.should.have.property('config');
+            obj.should.have.property('source');
+            obj.should.have.property('reload');
+        });
+
+        it('should not have settings before init', function() {
+            should.not.exist(obj.settings);
+        });
+
+        it('can setup config', function() {
+            obj.config().should.equal(obj);
+            obj._config.should.be.a('object');
+            obj.settings.should.be.a('object');
+        });
+
+        it('can use a source', function() {
+            obj.source(lorem).should.equal(obj);
+            obj._config.stack().should.eql([lorem]);
+        });
+
+        it('can reload', function() {
+            obj.reload().should.be.a('object');
+            obj.get('lorem').should.equal('ipsum');
+            obj.get('dolor').should.equal(true);
+            should.not.exist(obj.get('root'));
+        });
+
+        it('can use one more source', function() {
+            obj.source(ipsum).should.equal(obj);
+            obj._config.stack().should.eql([lorem, ipsum]);
+        });
+
+        it('can reload', function() {
+            obj.reload().should.be.a('object');
+            obj.get('lorem').should.equal('ipsum');
+            obj.get('dolor').should.equal(false);
+            obj.get('root').should.equal('{root}');
+        });
+
+        it ('can have different eson plugins', function() {
+            obj.esonPlugins.should.be.a('function');
+            obj.esonPlugins = function() {
+                return [eson.replace('{root}', root)];
+            };
+        });
+
+        it('can reload', function() {
+            obj.config().should.equal(obj);
+            obj.reload().should.be.a('object');
+            obj.get('lorem').should.equal('ipsum');
+            obj.get('dolor').should.equal(false);
+            obj.get('root').should.equal(root);
+        });
+    });
+});
