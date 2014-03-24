8e5d31616fc80efa392ee6cf4d82228a218d657d
Makara Wang <makara15@gmail.com>
2014-03-24 11:27:15 +0800

    Added _.wrapInvoke as an extension to highland.

--- src/highland.coffee
+++ src/highland.coffee
@@ -1,1 +1,28 @@
-module.exports = require('highland')
+module.exports = _ = require('highland')
+
+invoke = require('es5-ext/lib/Function/invoke')
+
+###*
+ * A wrap of es5-ext's invoke().
+ *
+ * Similar to _.wrapCallback(f) with the difference that this can be used to
+ *   invoke the function with a context (`this` in the function).
+ *
+ * This has a same syntax with invoke(); you invoke a function `func([0 to
+ *   many arguments], done)` with `_.wrapInvoke(func, [the arguments without
+ *   the done])(context or null)` and it will return a stream.
+ *
+ * @return {Function} the wrapped function
+###
+_.wrapInvoke = (args...) ->
+    ###*
+     * @return {Object} a stream that yields the result when callback is invoked
+    ###
+    return (context) ->
+        return _((push) ->
+            args.push((err, res) ->
+                if err then push(err) else push(null, res)
+                push(null, nil)
+            )
+            invoke(args...)(context)
+        )
---
+++ test/carcass.highland.mocha.js
@@ -0,0 +1,74 @@
+// var debug = require('debug')('carcass:test');
+
+var should = require('should');
+var highland = require('../lib/highland');
+var isNumber = require('es5-ext/lib/Number/is-number');
+
+describe('Carcass / highland:', function() {
+
+    it('should be a function', function() {
+        highland.should.be.type('function');
+    });
+
+    describe('wrapInvoke:', function() {
+
+        var plusOne = function(num, done) {
+            if (!isNumber(num)) {
+                return done(new TypeError('must be a number'));
+            }
+            done(null, num + 1);
+        };
+
+        var obj = {};
+        obj.count = 0;
+        obj.plusOne = function(done) {
+            this.count++;
+            done();
+        };
+
+        it('should be a function', function() {
+            highland.should.have.property('wrapInvoke').with.type('function');
+        });
+
+        it('can wrap a function', function(done) {
+            var invoke = highland.wrapInvoke(plusOne, 1);
+            invoke.should.be.type('function');
+            var stream = invoke();
+            stream.should.be.type('object');
+            stream.should.have.property('pull').with.type('function');
+            stream.pull(function(err, res) {
+                should.not.exist(err);
+                res.should.equal(2);
+                done();
+            });
+        });
+
+        it('can wrap a function and handler error', function(done) {
+            var invoke = highland.wrapInvoke(plusOne, 'lorem');
+            invoke.should.be.type('function');
+            var stream = invoke();
+            stream.should.be.type('object');
+            stream.should.have.property('pull').with.type('function');
+            stream.pull(function(err, res) {
+                should.exist(err);
+                should.not.exist(res);
+                done();
+            });
+        });
+
+        it('can wrap a method of an object', function(done) {
+            var invoke = highland.wrapInvoke(obj.plusOne);
+            invoke.should.be.type('function');
+            var stream = invoke(obj);
+            stream.should.be.type('object');
+            stream.should.have.property('pull').with.type('function');
+            stream.pull(function(err) {
+                should.not.exist(err);
+                obj.count.should.equal(1);
+                done();
+            });
+        });
+
+    });
+
+});
